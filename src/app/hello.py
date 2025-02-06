50a0608e06288262d2aef4ddf691ac7ad0cd7ebc
Einar Waara <94waara@gmail.com>
2025-02-06 11:47:20 +0100

    Fixed format issue

--- src/app/hello.py
+++ src/app/hello.py
@@ -3,8 +3,8 @@ def gen_set() -> set[int]:
 
 
 def main():
-    print('Hello from hello.py')
+    print("Hello from hello.py")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
