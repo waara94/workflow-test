7c393df775390c6d9a2302ff9dd3c82c411c7a0c
Einar Waara <94waara@gmail.com>
2025-02-06 16:41:18 +0100

    Fix nox

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,9 @@
 import nox
 
-py_versions = ["3.11", "3.12"]
-
+py_versions = ['3.11', '3.12']
 
 @nox.session(python=py_versions)
 def tests(session):
-    session.run("pytest -v --durations=0 --cov --cov-report=xml")
+    # session.run('uv', 'pip', 'sync', 'pyproject.toml', external=True)
+    session.run('uv', 'pip', 'install', 'pytest', 'pytest-cov', external=True)
+    session.run('pytest', '-v', '--durations=0', '--cov', '--cov-report=json')
