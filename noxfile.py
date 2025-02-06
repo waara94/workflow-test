6ecdbb90799ae1e3434e093effedf849a3113d21
Einar Waara <94waara@gmail.com>
2025-02-06 13:48:47 +0100

    New attempt

--- noxfile.py
+++ noxfile.py
@@ -5,4 +5,4 @@ py_versions = ["3.10", "3.11", "3.12"]
 
 @nox.session(python=py_versions)
 def tests(session):
-    session.run("uv run pytest -v --durations=0 --cov --cov-report=xml")
+    session.run("pytest -v --durations=0 --cov --cov-report=xml")
