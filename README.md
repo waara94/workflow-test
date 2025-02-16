20ec3dedd3720a201534cb5ba40061cb1edfc2bc
Einar Waara <94waara@gmail.com>
2025-02-16 15:26:59 +0100

    update

---
+++ README.md
@@ -0,0 +1,13 @@
+# settings.json
+```json
+    "ruff.importStrategy": "useBundled",
+    "editor.defaultFormatter": "charliermarsh.ruff",
+    "editor.formatOnPaste": true,
+    "editor.formatOnSave": true,
+    "editor.formatOnSaveMode": "file",
+    "editor.codeActionsOnSave": {
+        "source.organizeImports": "always",
+        "source.fixAll": "always"
+    },
+    "files.autoSave": "onFocusChange",
+```
--- noxfile.py
+++ noxfile.py
@@ -1,11 +1,11 @@
 import nox
 
 # py_versions = ['3.11', '3.12']
-py_versions = ["3.12"]
+py_versions = ['3.12']
 
 
 @nox.session(python=py_versions)
 def tests(session):
     # session.run('uv', 'pip', 'sync', 'pyproject.toml', external=True)
-    session.run("uv", "pip", "install", "pytest", "pytest-cov", external=True)
-    session.run("pytest", "-v", "--durations=0", "--cov", "--cov-report=json")
+    session.run('uv', 'pip', 'install', 'pytest', 'pytest-cov', external=True)
+    session.run('pytest', '-v', '--durations=0', '--cov', '--cov-report=json')
